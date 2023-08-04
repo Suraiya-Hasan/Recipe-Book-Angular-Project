3cc94d82702c99bb032ab6fe7e04b6968c6a698e
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-04 14:20:17 +0600

    configuring route parameters, passing dynamic parameters to links

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,44 +4,47 @@ import { Recipe } from "./recipe.model";
 import { EventEmitter, Injectable } from '@angular/core';
 
 @Injectable()
-export class RecipeService{
+export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
-    private recipes:Recipe[] = [
+    private recipes: Recipe[] = [
         new Recipe(
             "Chicken Pizza",
             "A delicious chicken pizza with chicken, mozzarella cheese and mushroom",
             "https://images.unsplash.com/photo-1590947132387-155cc02f3212?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-        [
-            new Ingredient('chicken',1),
-            new Ingredient('cheese',1)
-        ]),
+            [
+                new Ingredient('chicken', 1),
+                new Ingredient('cheese', 1)
+            ]),
         new Recipe(
             "Pepperoni Pizza",
             "A delicious chicken pizza with pepperoni, mozzarella cheese and cheddar cheese",
             "https://images.unsplash.com/photo-1628840042765-356cda07504e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
             [
-                new Ingredient('chicken',1),
-                new Ingredient('cheese',1), 
-                new Ingredient('pepperoni',250, 'gm')
+                new Ingredient('chicken', 1),
+                new Ingredient('cheese', 1),
+                new Ingredient('pepperoni', 250, 'gm')
             ]),
         new Recipe(
             "Chicken Cheese Pizza",
             "A delicious chicken pizza with chicken, mozzarella cheese and mushroom",
             "https://images.unsplash.com/photo-1590947132387-155cc02f3212?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
             [
-                new Ingredient('chicken',1),
-                new Ingredient('cheese',1),
-                new Ingredient('cheddar',20,'gm')
+                new Ingredient('chicken', 1),
+                new Ingredient('cheese', 1),
+                new Ingredient('cheddar', 20, 'gm')
             ])
-      ];
+    ];
 
-    constructor(private slService:ShoppingListService){}
+    constructor(private slService: ShoppingListService) { }
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice();
     }
-    addIngToShoppingList(ingredients:Ingredient[]){
+    addIngToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
+    }
+    getRecipe(id: number) {
+        return this.recipes[id];
     }
 }
