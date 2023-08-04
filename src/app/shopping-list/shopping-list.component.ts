6106015e5243c1f2559ff7af3ec902cafdef43df
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-04 17:15:35 +0600

    using subjects instead of event emitters

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit{
-  ingredients!:Ingredient[];
-  constructor(private slService:ShoppingListService){}
+export class ShoppingListComponent implements OnInit, OnDestroy {
+  ingredients!: Ingredient[];
+  private igChangedSub!: Subscription;
+
+  constructor(private slService: ShoppingListService) { }
+
   ngOnInit(): void {
-      this.ingredients=this.slService.getIngredient();
-      this.slService.ingChanged.subscribe(
-        (ing:Ingredient[]) =>{
-          this.ingredients=ing;
-        }
-      )
+    this.ingredients = this.slService.getIngredient();
+    this.igChangedSub = this.slService.ingChanged.subscribe(
+      (ing: Ingredient[]) => {
+        this.ingredients = ing;
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.igChangedSub.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,22 +1,22 @@
 import { Ingredient } from "../shared/ingredient.model";
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
-export class ShoppingListService{
-    ingChanged = new EventEmitter<Ingredient[]>();
+export class ShoppingListService {
+  ingChanged = new Subject<Ingredient[]>();
 
-    private ingredients:Ingredient[] = [
-        new Ingredient('chicken',250, 'gm'),
-        new Ingredient('cheese',50, 'gm'),
-      ];
-      getIngredient(){
-        return this.ingredients.slice();
-      }
-      addIng(ingredient:Ingredient){
-        this.ingredients.push(ingredient);
-        this.ingChanged.emit(this.ingredients.slice());
-      }
-      addIngredients(ingredients: Ingredient[]){
-        this.ingredients.push(...ingredients);
-        this.ingChanged.emit(this.ingredients.slice())
-      }
+  private ingredients: Ingredient[] = [
+    new Ingredient('chicken', 250, 'gm'),
+    new Ingredient('cheese', 50, 'gm'),
+  ];
+  getIngredient() {
+    return this.ingredients.slice();
+  }
+  addIng(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingChanged.next(this.ingredients.slice());
+  }
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingChanged.next(this.ingredients.slice())
+  }
 }
