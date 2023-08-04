e29e1eeb2cd70bed40cce41aa309e249b32d40dc
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-04 15:27:59 +0600

    adding a new component for edit or new recipes and setting those routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { Dropdown } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 
 
 @NgModule({
@@ -27,7 +28,9 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
     ShoppingListComponent,
     ShoppingEditComponent,
     Dropdown,
-    RecipeStartComponent
+    RecipeStartComponent,
+    RecipeEditComponent,
+
   ],
   imports: [
     BrowserModule,
