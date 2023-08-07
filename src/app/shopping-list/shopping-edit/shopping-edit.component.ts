2880a70fcefff4f0cb5ba4c2501f0e934fc2c68b
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-07 14:23:09 +0600

    clearing fields and making the clear button work

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -38,10 +38,16 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount, value.unit);
     if (this.editMode) {
-      this.slService.updateIngredient(this.editedItemIndex, newIngredient)
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
     } else {
       this.slService.addIng(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
+  }
+  onClear() {
+    this.slForm.reset();
+    this.editMode = false;
   }
   ngOnDestroy(): void {
     this.subscription.unsubscribe()
