b949e29a35085899114f000391f3c16408d1b034
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-09 14:45:37 +0600

    fetching recipes from firebase

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RecipeService } from "../recipes/recipe.service";
+import { Recipe } from "../recipes/recipe.model";
+import { map } from 'rxjs/operators'
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -13,5 +15,15 @@ export class DataStorageService {
         this.http
             .put('https://recipe-book-3bb1f-default-rtdb.firebaseio.com/recipes.json', recipes)
             .subscribe(response => console.log(response));
+    }
+
+    fetchRecipes() {
+        this.http.get<Recipe[]>('https://recipe-book-3bb1f-default-rtdb.firebaseio.com/recipes.json').pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
+            })
+        })).subscribe(recipes => {
+            this.recipeService.setRecipes(recipes);
+        });
     }
 }
