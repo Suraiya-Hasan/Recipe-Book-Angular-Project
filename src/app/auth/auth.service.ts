27bf85c69ffa190fbd09c82eb880f2c062a77546
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-10 14:48:49 +0600

    error handling in login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs";
 import { throwError } from "rxjs";
@@ -26,19 +26,7 @@ export class AuthService {
                 password: password,
                 returnSecureToken: true
             }
-        ).pipe(
-            catchError(errorRes => {
-                let errorMessage = 'An unknown error occurred.'
-                if (!errorRes.error || !errorRes.error.error) {
-                    return throwError(errorMessage);
-                }
-                switch (errorRes.error.error.message) {
-                    case 'EMAIL_EXISTS':
-                        errorMessage = "This Email already exists";
-                }
-                return throwError(errorMessage);
-            })
-        );
+        ).pipe(catchError(this.handleError));
     }
     login(email: string, password: string) {
         return this.http.post<AuthResData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.API_KEY}`,
@@ -48,5 +36,24 @@ export class AuthService {
                 returnSecureToken: true
             }
         )
+            .pipe(catchError(this.handleError));
+    }
+    private handleError(errorRes: HttpErrorResponse) {
+        let errorMessage = 'An unknown error occurred.'
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = "This Email already exists";
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = "Login credentials don't match.";
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = "Login credentials don't match.";
+                break;
+        }
+        return throwError(errorMessage);
     }
 }
