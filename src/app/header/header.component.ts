b8cd338b3b9ceb8656961974e6f1e810168b9a5b
Suraiya-Hasan <suraiyahasan@iut-dhaka.edu>
2023-08-10 16:21:29 +0600

    reflecting authenticated state in the UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from "@angular/core";
 import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "../auth/auth.service";
+import { Subscription } from "rxjs";
 
 @Component({
     selector: "app-header",
@@ -7,12 +9,28 @@ import { DataStorageService } from "../shared/data-storage.service";
     styleUrls: ["./header.component.css"]
 })
 
-export class HeaderComponent {
-    constructor(private dataStorageService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+
+    private userSub!: Subscription;
+    isAuthenticated = false;
+
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
+
     onSaveData() {
         this.dataStorageService.storeRecipes();
     }
+
     onFetchData() {
         this.dataStorageService.fetchRecipes().subscribe();
+    }
+
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe(user => {
+            this.isAuthenticated = !!user;
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
     }
 }
